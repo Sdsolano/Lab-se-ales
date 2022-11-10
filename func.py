9fbab035afb705e494f7e41c6b081cfbf31d9106
Sdsolano <114268063+Sdsolano@users.noreply.github.com>
2022-11-10 15:30:06 -0500

    Update func.py

--- func.py
+++ func.py
@@ -46,27 +46,13 @@ def serief(n,t,y,T,p):
 
     return t1,xf,c,fase,t5,a0
 
-def señalc(w0,w1,w2,fs,A0,A1,A2):
-    Ts=1/fs  
-    t=np.arange(0,1,Ts)
-    y=A0*np.sin(w0*t) + A1*np.cos(w1*t) + A2*np.sin(w2*t)
-    return t,y
-
-def señald(w0,w1,w2,fs,A0,A1,A2,n):
-    t=np.arange(0,n)
-    y=A0*np.sin(2*np.pi*w0*t/fs) + A1*np.cos(2*np.pi*w1*t/fs) + A2*np.sin(2*np.pi*w2*t/fs)
-    return t,y
 
 def tf(y,fs):
-    ft=np.fft.fft(y)
-    oft=sp.fft.fftshift(ft) 
+    ft=np.fft.fft(y) 
     hzft=abs(ft)
-    phift=np.angle(oft)
+    phift=np.angle(ft)
     t1=np.fft.fftfreq(len(y))*fs
-     #t1=fs*np.arange(-0.5-(1/len(hzft)),0.5-(1/len(hzft)),(1/len(hzft)))
-    t2=fs*np.arange(-0.5-(1/len(phift)),0.5-(1/len(phift)),(1/len(phift)))
-    return t1,t2,hzft,phift
+    return t1,hzft,
 
-plt.plot(t1,ysf,'g')
-plt.plot(r,k,'r--')
+plt.plot(t1,hzft,'g')
 plt.show()
